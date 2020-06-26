ea31138acb673dfe91a80b027e25e3f8ae822075
unknown <arunareath@gmail.com>
2020-06-26 15:47:41 +0530

    Deal page 2nd commit

--- src/main/java/com/qa/page/DealPage.java
+++ src/main/java/com/qa/page/DealPage.java
@@ -1,5 +1,11 @@
 package com.qa.page;
 
-public class DealPage {
+import com.qa.base.BasePage;
 
+public class DealPage extends BasePage {
+
+	public DealPage()
+	{
+		System.out.println("Deal page");
+	}
 }
