d24aee36d0edd66a9385218cc1d320474d0e0e3c
unknown <arunareath@gmail.com>
2020-06-28 07:05:26 +0530

    added more TC

--- src/main/java/com/qa/util/Constants.java
+++ src/main/java/com/qa/util/Constants.java
@@ -2,7 +2,7 @@ package com.qa.util;
 
 public class Constants {
 	
-	public static String Login_page_title="HubSpot Login123";
+	public static String Login_page_title="HubSpot Login";
 	public static String Home_page_title="Account Setup | HubSpot";
 	public static int time=20;
 
